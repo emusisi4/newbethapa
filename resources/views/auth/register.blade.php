4b84d5b37e6ac9233fa225a864d6637f465781d5
emusisi4 <emusisi4@gmail.com>
2021-06-22 00:51:46 +0300

    4rd  commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+                    <!-- <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
                         @csrf
 
                         <div class="form-group row">
@@ -68,10 +68,10 @@
                                 </button>
                             </div>
                         </div>
-                    </form>
+                    </form> -->
                 </div>
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', function () { return view('/auth/login');});
 
 Auth::routes();
 Route::get('{path}',"HomeController@index")->where('path','[-a-z0-9_\s]+');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'HomeController@index')->name('dashboard');
