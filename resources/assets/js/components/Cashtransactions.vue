9f13e1eb5addcdd8c00e062c9732334de35fccea
emusisi4 <emusisi4@gmail.com>
2021-08-01 21:54:53 +0300

    new gng  upload

--- resources/assets/js/components/Cashtransactions.vue
+++ resources/assets/js/components/Cashtransactions.vue
@@ -1315,7 +1315,7 @@ window.onload = function(){
 
 <div v-if=" virtualgameproduct > 0  ">
 
-<div class ="bethapa-table-header">VIRTUAL DETAILS</div>
+<div class ="bethapa-table-header">VIRTUAL DETAIbbfffffffffffLS</div>
                        
  <div class="form-group row">
 
@@ -1331,7 +1331,7 @@ window.onload = function(){
 
                             <label class="col-sm-2 col-form-label">Cancelled </label>
                               <div class="col-sm-6">
-                          <input v-model="form.vcan" type="text" name="vcan"
+                          <input v-model="form.vcan" value="0" readonly="true" type="text" name="vcan"
         class="form-control" :class="{ 'is-invalid': form.errors.has('vcan') }">
       <has-error :form="form" field="vcan"></has-error>
                               </div>
@@ -1759,7 +1759,7 @@ window.onload = function(){
 
                             <label class="col-sm-2 col-form-label">Cancelled </label>
                               <div class="col-sm-6">
-                          <input v-model="form.vcan" type="text" name="vcan"
+                          <input v-model="form.vcan"  readonly="true" type="text" name="vcan"
         class="form-control" :class="{ 'is-invalid': form.errors.has('vcan') }">
       <has-error :form="form" field="vcan"></has-error>
                               </div>
@@ -3218,6 +3218,7 @@ contact:'',
 location:'',
 expense:'',
 description:'',
+vcan:'0',
 ///
  receiptno : '',
                                     branch : '',
